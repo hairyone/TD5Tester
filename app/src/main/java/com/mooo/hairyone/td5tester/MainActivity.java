ad3ad417a0df866702c93e959b47ca193a1be2ea
Hairyone <aaron.trewern@gmail.com>
2017-10-19 14:38:53 +0100

    More tweaks

--- app/src/main/java/com/mooo/hairyone/td5tester/MainActivity.java
+++ app/src/main/java/com/mooo/hairyone/td5tester/MainActivity.java
@@ -1,18 +1,20 @@
 package com.mooo.hairyone.td5tester;
 
-import android.support.v7.app.AppCompatActivity;
+// import android.support.v7.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+
 import com.ftdi.j2xx.D2xxManager;
 import com.ftdi.j2xx.FT_Device;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     D2xxManager ftdid2xx = null;
     FT_Device ftDevice = null;
@@ -43,13 +45,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void log_append(String text) {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        // SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss.SSS");
         Date now = new Date();
         LoggingTextView.append(String.format("%s : %s\n", sdf.format(now), text));
     }
 
     public void connect() {
-        Toast.makeText(getApplicationContext(), "Connecting ...", Toast.LENGTH_SHORT).show();
+        // Toast.makeText(getApplicationContext(), "Connecting ...", Toast.LENGTH_SHORT).show();
         log_clear();
         try {
             ftdid2xx = D2xxManager.getInstance(getApplicationContext());
@@ -63,6 +66,12 @@ public class MainActivity extends AppCompatActivity {
 
                 log_append(String.format("serial_number=%s", deviceList[0].serialNumber == null ? "null" : deviceList[0].serialNumber));
                 log_append(String.format("description=%s", deviceList[0].description == null ? "null" : deviceList[0].description));
+                log_append(String.format("location=%d", deviceList[0].location));
+                log_append(String.format("id=%s", deviceList[0].id));
+                log_append(String.format("library_version=%d", D2xxManager.getLibraryVersion()));
+                log_append(String.format("type=%d", deviceList[0].type));
+                log_append(String.format("type_name=%s", get_device_type(deviceList[0].type)));
+
             }
 
         } catch (D2xxManager.D2xxException e) {
@@ -70,6 +79,23 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+    }
+
+    public String get_device_type(int device_type) {
+        String result = "unknown device";
+        switch (device_type) {
+            case D2xxManager.FT_DEVICE_232B: result = "FT232B"; break;
+            case D2xxManager.FT_DEVICE_8U232AM: result = "FT8U232AM"; break;
+            case D2xxManager.FT_DEVICE_UNKNOWN: result = "Unknown"; break;
+            case D2xxManager.FT_DEVICE_2232: result = "FT2232"; break;
+            case D2xxManager.FT_DEVICE_232R: result = "FT232R"; break;
+            case D2xxManager.FT_DEVICE_2232H: result = "FT2232H"; break;
+            case D2xxManager.FT_DEVICE_4232H: result = "FT4232H"; break;
+            case D2xxManager.FT_DEVICE_232H: result = "FT232H"; break;
+            case D2xxManager.FT_DEVICE_X_SERIES: result = "FTDI X_SERIES"; break;
+            default: result = "FT232B"; break;
+        }
+        return result;
     }
 
 }
--- app/src/test/java/com/mooo/hairyone/td5tester/ExampleUnitTest.java
+++ app/src/test/java/com/mooo/hairyone/td5tester/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.mooo.hairyone.td5tester;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
