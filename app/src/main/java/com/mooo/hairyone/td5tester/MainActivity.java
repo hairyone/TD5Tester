c38f0079497eb2a8fcdd28124a4e798aab3c2410
Hairyone <aaron.trewern@gmail.com>
2017-10-19 10:22:59 +0100

    initial commit

---
+++ app/src/main/java/com/mooo/hairyone/td5tester/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mooo.hairyone.td5tester;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
